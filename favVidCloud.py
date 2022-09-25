d645e23a76d186996e1eb551a60362f37cf0afe7
Brian <bmccormack15@gmail.com>
2022-09-25 03:41:13 -0700

    added menu printing and input parse still in progress

--- favVidCloud.py
+++ favVidCloud.py
@@ -1,7 +1,38 @@
-!#!/usr/bin/python3
+#!/usr/bin/python3
 
-#use file io to permantly store items
-def listFavourites():
+#use file io to permantly store items later
+#	for now just use a dictionary
 
-def playVideo(app, url):
-	
+favs = {}
+
+def printMenu():
+	print("Would you like to: ")
+	print("1: Play Video by URL")
+	print("2: Play Favourite Video")
+	print("3: Manage Favourites")
+	print("4: Quit")	
+
+def menu():
+	printMenu()
+	return parseInput(input("Choice: "))
+
+#assuming properly formatted	
+def parseInput(option):
+	return option
+		
+def listVideos():
+	return
+def playOnlineVideo(app, url):
+	return
+def playFavouriteVideo(app, url):
+	return
+def playVideo():
+	return
+def addVideo(url):
+	return
+
+playerChoice = "mpv"
+while True:
+	selection = menu()
+	if selection == "4":
+		break	
